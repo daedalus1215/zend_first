f6f94d2db1b60cd9394add52e9eb79b9e27c4f83
Larry <ladams1776@yahoo.com>
2016-05-14 18:51:29 -0400

    Added getServiceConfig to Module.php which sets up a ServiceManager to configure the table gateway and inject into the AlbumTable

--- module/Album/Module.php
+++ module/Album/Module.php
@@ -39,4 +39,41 @@ class Module implements AutoloaderProviderInterface, ConfigProviderInterface
     return include __DIR__ . '/config/module.config.php';
   }
 
+  /*
+   * This method returns an array of factories that are all merged 
+   * together by the ModuleManager before passing them to the ServiceManager. 
+   */
+  public function getServiceConfig() 
+  {
+      return array(
+          'factories' => array(
+            /* The factory for Album\Model\AlbumTable uses the ServiceManager to 
+             * create an AlbumTableGateway to pass to the AlbumTable. 
+             */
+            'Album\Model\AlbumTable' => function ($sm) {
+                $tableGateway = $sm->get('AlbumTableGateway');
+                $table = new AlbumTable($tableGateway);
+                return $table;
+              },
+                      
+            /* Tell the ServiceManager that an AlbumTableGateway is created by 
+             * getting a Zend\Db\Adapter\Adapter (also from the ServiceManager)
+             * and using it to create a TableGateway object. 
+             * 
+             * The TableGateway is told to use an Album object whenever it creates 
+             * a new result row. The TableGateway classes use the protoype pattern 
+             * for creation of result sets and entities. 
+             * 
+             * This means that instead of instantiating when required, the system 
+             * clones a previously instantiated object.
+             */
+            'AlbumTableGateway' => function ($sm) {
+                  $dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
+                  $resultSetPrototype = new ResultSet();
+                  $resultSetPrototype->setArrayObjectPrototype(new Album());
+                  return new TableGateway('album', $dbAdapter, null, $resultSetPrototype);
+            },
+          ),
+      );
+  }
 }
