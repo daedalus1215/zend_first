571089821ddddc4ad08b4419c6ed39eaaddbff61
Laurence Adams <ladams1776@yahoo.com>
2016-05-20 00:37:51 -0400

    building up AlbumForm

---
+++ module/Album/src/Album/Form/AlbumForm.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Album\Form;
+
+use Zend\Form\Form;
+
+class ALbumForm extends Form 
+{
+    public function __constructor($name = null) 
+    {
+        // we want to ignore the name passed
+        parent::__contruct('album');
+        
+        
+        
+        /*
+            The first thing we do is set the name of the form, as we call the parent's constructor. We create four form elements: the id, title, artist, and submit button. For each of the items we set various attributes and options, including the label to be displayed.
+        
+        */
+        $this->add(array(
+            'name' => 'id',
+            'type' => 'Hidden'
+        ));
+        
+        $this->add(array(
+            'name' => 'title',
+            'type' => 'Text',
+            'options' => array(
+                'label' => 'Title'
+            )
+        ));
+        
+        $this->add(array(
+            'name' => 'artist',
+            'type' => 'Text',
+            'options' => array(
+                'label' => 'Artist'
+            ),
+        ));
+        
+        $this->add(array(
+            'name' => 'submit',
+            'type' => 'Submit',
+            'attributes' => array(
+                'value' => 'Go',
+                'id' => 'submitbutton'
+            ),
+        ));
+    }
+}
