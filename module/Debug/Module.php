659b5d670adb416729072f424bf2c20ecdb0254d
Larry <ladams1776@yahoo.com>
2016-06-05 20:48:01 -0400

    Kick off a timer, in onBootstrap and setup a listener to the finish event, then spit out the amount of time it took to respond to a request.

--- module/Debug/Module.php
+++ module/Debug/Module.php
@@ -35,19 +35,26 @@ class Module implements AutoloaderProviderInterface
     
     public function onBootstrap(MvcEvent $e)
     {
-//        $eventManager = $e->getApplication()->getEventManager();
-//        $eventManager->attach(MvcEvent::EVENT_DISPATCH_ERROR, array($this, 'handleError'));
-        
         $eventManager = $e->getApplication()->getEventManager();
-        $sharedEventManager = $eventManager->getSharedManager();
-        $sharedEventManager->attach('channel-25', 'new song', function(Event $event) {
-            $artist = $event->getParam('artist');
-            error_log('Got new song from artist:'.$artist);
-            
-        });
+        $eventManager->attach(MvcEvent::EVENT_DISPATCH_ERROR
+                            , array($this, 'handleError'));
+        
+        // Access the ServiceManager
+        $serviceManager = $e->getApplication()->getServiceManager();
+        // Here we start the timer
+        $timer = $serviceManager->get('timer');
+        $timer->start('mvc-execution');
+        
+        
+        // attach listener to the finish event that has to be executed with priority 2
+        // The priority here is 2 because listeners with the priority will be executed just before the 
+        // actual finish event is triggered.
+        $eventManager->attach(MvcEvent::EVENT_FINISH
+                            , array($this, 'getMvcDuration')
+                            , 2);
     }
     
-    
+ 
     public function handleError(MvcEvent $e)
     {
         $controller = $e->getController();
@@ -59,5 +66,16 @@ class Module implements AutoloaderProviderInterface
         }
         
         error_log($message);
-    }    
+    }  
+    
+    public function getMvcDuration(MvcEvent $event) 
+    {
+        // Here we get the ServiceManager.
+        $serviceManager = $event->getApplication()->getServiceManager();
+        // Get the already created instance of our timer service.
+        $timer = $serviceManager->get('timer');
+        $duration = $timer->stop('mvc-execution');
+        // finally print the duration
+        error_log('MVC Duration: ' . $duration . ' seconds');
+    }
 }
