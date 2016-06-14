7141a1dccfae5b4edd5d4dfc19345456f8662c15
Larry <ladams1776@yahoo.com>
2016-06-13 21:35:45 -0400

    changes rdy to be merged

--- module/User/src/User/Controller/AccountController.php
+++ module/User/src/User/Controller/AccountController.php
@@ -14,81 +14,20 @@ use User\Model\User as UserModel;
 
 class AccountController extends AbstractActionController
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
-        public function indexAction()
-    {
-        return array();
-    }
-    public function addAction()
-    {
-        return array();
-    }
-    /*
-   * Anonymous users can use this action to register new accounts
-   */
-    public function registerAction()
-    {
-        return array();
-    }
-    public function viewAction()
-    {
-        return array();
-    }
-    public function editAction()
-    {
-        return array();
-    }
+    /**
+     * Delete a User.
+     * @return Array
+     */
     public function deleteAction()
     {
+        $id = $this->getRequest()->getQuery()->get('id');
+        if ($id) {
+            $userModel = new UserModel();
+            $userModel->delete(array('id' => $id));
+        }
+
         return array();
     }
-
-=======
-   public function addAction()
-   {
-       $form = new UserForm();             
-
-       if ($this->getRequest()->isPost()) {
-          // notice: make certain to merge the Files also to the post data.
-          $data = array_merge_recursive(
-            $this->getRequest()->getPost()->toArray(),
-            //The post data will contain all post parameters except the file uploads. In order to get them from the request we have to use getFiles().
-            $this->getRequest()->getFiles()->toArray()
-          );
-
-           $form->setData($data);
-
-           if ($form->isValid()) {
-             // @todo: save the data of the new user.
-             $model = new UserModel();
-             // getData() method will return an array of key/value pairs. The 
-             // names of the table columns will be taken from the keys, and the 
-             // values will be the value portions of the array.
-             $id = $model->insert($form->getData());
-             // @todo: redirect user to the view user action.
-           }
-       }
-
-       // pass the data to the view for visualization.
-       return array('form' => $form);
-   }
-   
-   public function deleteAction()
-   {
-     $id = $this->getRequest()->getQuery()->get('id');
-     if ($id) {
-       $userModel = new UserModel();
-       $userModel->delete(array('id' => $id));
-     }
-     
-     return array();
-   }
-   
-   
->>>>>>> b3214109f932b263586a4b0e121328c49c30cd05
-}
-=======
 
 
     /**
@@ -135,22 +74,6 @@ class AccountController extends AbstractActionController
 
 
     /**
-     * Delete a User.
-     * @return Array
-     */
-    public function deleteAction()
-    {
-        $id = $this->getRequest()->getQuery()->get('id');
-        if ($id) {
-            $userModel = new UserModel();
-            $userModel->delete(array('id' => $id));
-        }
-
-        return array();
-    }
-
-
-    /**
      * Anonymous users can use this action to register new accounts
      * @return URL
      */
@@ -189,7 +112,5 @@ class AccountController extends AbstractActionController
         }
 
     }
-
-
 }
->>>>>>> databse_profiler
+
